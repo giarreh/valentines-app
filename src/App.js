72e8feff354d7b73699cf82689c0446309ec4c3c
Giar Rehani <giarreh1@gmail.com>
2024-02-08 19:42:18 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import Confetti from 'react-confetti';
 
 function App() {
+  const [buttonPosition, setButtonPosition] = useState({ x: 40, y: 40 });
+  const [fadeIn, setFadeIn] = useState(false);
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [h1Text, setH1Text] = useState('Will you be my valentine?');
+
+  const handleButtonHover = () => {
+    const randomX = Math.random() * (window.innerWidth - 200);
+    const randomY = Math.random() * (window.innerHeight - 50);
+    setButtonPosition({ x: randomX, y: randomY });
+  };
+  const handleYesButtonClick = () => {
+    console.log("CLICKED!");
+    setH1Text('I love you!❤️');
+    setShowConfetti(true);
+  };
+  const handleNoButtonClick = () => {
+    console.log("CLICKED!");
+    setH1Text('I still love you, but now im very sad 😢');
+    setShowConfetti(false);
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setFadeIn(true);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const noButtonStyle = {
+    position: 'absolute',
+    left: `${buttonPosition.x}px`,
+    top: `${buttonPosition.y}px`,
+    opacity: fadeIn ? 1 : 0,
+    transition: 'opacity 1s ease-in-out, left 0.3s ease-in-out, top 0.3s ease-in-out'
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1 className='title'>Hello Kinja!❤️</h1>
+      <div className='content'>
+      <h1>{h1Text}</h1>
+        <div className='buttons'>
+          <div className='yesButton' onClick={handleYesButtonClick}>Yes</div>
+          <div className='noButton' style={noButtonStyle} onMouseOver={handleButtonHover} onClick={handleNoButtonClick}>No</div>
+        </div>
+      </div>
+      {showConfetti && <Confetti />}
+    </main>
   );
 }
 
